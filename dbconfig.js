bc85006a94256af15c6ed4a592089338a5509ac2
SengulC <98541035+SengulC@users.noreply.github.com>
2026-04-21 16:15:10 +0100

    timezone fix

--- dbconfig.js
+++ dbconfig.js
@@ -5,6 +5,7 @@ module.exports = {
     port: 3306,  
     user: '2526-cs7025-group2',
     password: process.env.DB_PW,
+    timezone: '+00:00',
     database: '2526-cs7025-group2_db',
     connectionLimit: 5,
     bigIntAsNumber: true // to fix bigint stuff
